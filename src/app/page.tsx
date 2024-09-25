66584f6fcd7451a0ec0e05c2c9de0d978099b63a
Zeta Reticula <zetareticulae@proton.me>
2024-09-24 21:51:01 -0700

    new welcome
    sign rotation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,9 +22,9 @@ export default function Home() {
         {
           id: "0",
           role: "system",
-          content: `**Welcome to DegreeGuru**
+          content: `**Welcome to Klever**
 
-Your ultimate companion in navigating the academic landscape of Stanford.`,
+Your ultimate companion`,
         },
       ],
       onResponse: () => {
